08d657b8871f76ff88d16c5a77635b3c6961422f
SDemianyk02 <serhii.demianyk@nocoderebels.dev>
2025-05-19 09:07:20 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ export const config = {
   patterns: [
       {
           pattern: "/?post=+",
-          metaDataEndpoint: "https://xsrr-l2ye-dpbj.f2.xano.io/api:Coq7oZJp:live/posts/get_single/{post_id}"
+          metaDataEndpoint: "https://xsrr-l2ye-dpbj.f2.xano.io/api:Coq7oZJp/posts/get_single/{post_id}"
       },
       // {
       //     pattern: "/team/profile/[^/]+",
